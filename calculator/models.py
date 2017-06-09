09a66e2df85630572f3b6dad102ba5e034cc7d39
Jonathan Le Bonzec <jonathan.lucky13@gmail.com>
2017-06-09 18:34:10 +0000

    Improved models
    - Added signals to serialize/deserialize the middle_names list
    - Added flag for the result edition/version management
    - Added __repr__ and __str__ when appropriate
    - Added docstrings
    - Added anaphora to commit message

--- calculator/models.py
+++ calculator/models.py
@@ -1,53 +1,145 @@
 from django.db import models
+from django.dispatch import receiver
 
-# Create your models here.
+import datetime
+import json
 
 # TODO: names and __str__ / __repr__
 
+""" Models and Signals used during the calculation
+
+Meanings:
+- User: the physical person
+- Number: the name of the computable digit (i.e. "Life path", "Master number", etc)
+- Template: generic explanation of a value obtained by a Number computation (see above)
+- Result: Personalized explanation of a value obtained by a Number computation
+"""
+
 
 class User(models.Model):
+    """ The user model. This model corresponds to the physical person asking for a study.
+    """
     first_name = models.CharField(max_length=80, null=False, blank=False)
-    middle_name = models.CharField(max_length=160, default='', null=False, blank=True)
+    middle_names = models.CharField(max_length=160, default='[]', null=False, blank=True)
     last_name = models.CharField(max_length=80, null=False, blank=False)
     birth = models.DateField(null=False, blank=False)
 
     class Meta:
-        unique_together = ('first_name', 'middle_name', 'last_name', 'birth')
-        index_together = ('first_name', 'middle_name', 'last_name', 'birth')
+        unique_together = ('first_name', 'middle_names', 'last_name', 'birth')
+        index_together = ('first_name', 'middle_names', 'last_name', 'birth')
+
+    def __repr__(self):
+        names = [self.first_name]
+        names += self.middle_names
+        names = [name.title() for name in names]
+        return "<User: %s %s (%s)>" % (', '.join(names), self.last_name.upper(), self.birth)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name.title(), self.last_name.upper())
 
 
-# TODO: change class name
 class Number(models.Model):
+    """ The Number model. It contains shortened names and descriptions corresponding to
+    computable Numbers.
+
+    example: Life Path pattern Number is a computable Number
+    """
     code = models.CharField(max_length=20, primary_key=True)
     name = models.CharField(max_length=120, null=False, blank=False)
     description = models.TextField(default='', null=False, blank=True)
 
+    def __repr__(self):
+        return "<Number: %s>" % self.code
+
+    def __str__(self):
+        return self.name
+
 
 class AbstractContentModel(models.Model):
+    """ Abstract model representing an explanation of the result of a computable Number
+
+    example: The person obtained 3 to a number computation:
+        value = 3
+        explanation = "That's a great result!"
+    """
     value = models.SmallIntegerField(max_length=2, null=False)
     explanation = models.TextField(default='', null=False, blank=True)
 
     class Meta:
         abstract = True
 
+    def __str__(self):
+        return self.explanation
+
 
 class Template(AbstractContentModel):
-    # TODO: rename this field below
-    code = models.ForeignKey(Number, on_delete=models.CASCADE,
-                             related_name='templates', related_query_name='template')
+    """ Generic explanation of a value obtained by a computation
+    """
+    number = models.ForeignKey(Number, on_delete=models.CASCADE,
+                               related_name='templates', related_query_name='template')
 
     class Meta:
-        unique_together = ('code', 'value')
-        index_together = ('code', 'value')
+        unique_together = ('number', 'value')
+        index_together = ('number', 'value')
+
+    def __repr__(self):
+        return "<Template: [%s, %s]>" % (self.number.code, self.value)
 
 
 class Result(AbstractContentModel):
+    """ Personalized explanation of a value obtained by a computation.
+
+    It's linked to a user, and is versioned through dates
+    """
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name='results', related_query_name='result')
-    code = models.ForeignKey(Number, on_delete=models.CASCADE,
-                             related_name='results', related_query_name='result')
-    date = models.DateField(null=False, blank=False)
+    number = models.ForeignKey(Number, on_delete=models.CASCADE,
+                               related_name='results', related_query_name='result')
+    date = models.DateTimeField(null=False, blank=False)  # This should never be directly edited
+
+    __original_explanation = None
+
+    def __init__(self, *args, **kwargs):
+        super(Result, self).__init__(*args, **kwargs)
+        self.__original_explanation = self.explanation
 
     class Meta:
-        unique_together = ('user', 'code', 'value', 'date')  # include date to keep history
-        index_together = ('user', 'code', 'value', 'date')  # include date to keep history
+        unique_together = ('user', 'number', 'value', 'date')  # include date to keep history
+        index_together = ('user', 'number', 'value', 'date')  # include date to keep history
+
+    def __repr__(self):
+        return "<Result: %s - [%s, %s]>" % (self.user, self.number.code, self.value)
+
+    def save(self, *args, **kwargs):
+        """ Override the save method to properly handle versions
+        """
+        if not self.date or self.__original_explanation is not self.explanation:
+            # Actually update the record
+            now = datetime.datetime.now()
+            super(Result, self).save(*args, **kwargs)
+            self.__original_explanation = self.explanation
+            self.date = now
+
+
+# ------------- #
+# -- Signals -- #
+
+@receiver(models.signals.post_init, sender=User)
+@receiver(models.signals.post_save, sender=User)
+def serialize_user(sender, **kwargs):
+    """ Transform the middle names list into json
+    """
+    obj = kwargs['instance']
+    try:
+        obj.middle_names = json.loads(obj.middle_names)
+    except TypeError:
+        # Probably first initialization, ignore
+        pass
+
+
+@receiver(models.signals.pre_save, sender=User)
+def deserialize_user(sender, **kwargs):
+    """ Transform the middle names json into a list
+    """
+    obj = kwargs['instance']
+    obj.middle_names = json.dumps(obj.middle_names)
