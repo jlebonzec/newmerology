jlebonzec/newmerology

commit f376e49958cad1b2d43238653f3d4e9072a1910e

Fix: changed Method → Computation in models

calculator/models.py
@@ -127,7 +127,7 @@ class Number(models.Model):
             if self.computation_method:
                 self.computation = import_module(
                     COMPUTATION_METHODS_PATH + self.computation_method
-                ).Method(self._person)
+                ).Computation(self._person)
 
     @property
     def value(self):

commit 108c08c04c9a1d42016744081b4e7f7cadb4e0af

Fix: computation.value → computation.result
Bug introduced in f376e49

calculator/models.py
@@ -148,16 +148,16 @@ class Number(models.Model):
         if not self.computation:
             return
         if self.person and any((force, self._template is None, self._result is None)):
-            self._result = self.results.filter(person=self.person, value=self.computation.value).first()
+            self._result = self.results.filter(person=self.person, value=self.computation.result).first()
             if self._result is None:
                 self._result = Result(number=self,
                                       person=self.person,
-                                      value=self.computation.value)
+                                      value=self.computation.result)
                 self._result.save()
 
-            self._template = self.templates.filter(value=self.computation.value).first()
+            self._template = self.templates.filter(value=self.computation.result).first()
             if self._template is None:
-                self._template = Template(number=self, value=self.computation.value)
+                self._template = Template(number=self, value=self.computation.result)
                 self._template.save()
 
 

calculator/templates/calculator/person.djt
@@ -27,7 +27,7 @@
                 </label>
 
                 <span class="number-circle" >
-                    {{ number.computation.value }}
+                    {{ number.computation.result }}
                 </span>
             </h3>
 