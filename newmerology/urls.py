c84c05ab6a3f6a7a99f84bc6dbbbb6e3391c466d
Jonathan Le Bonzec <jonathan.lucky13@gmail.com>
2017-09-18 16:17:22 +0200

    Removed markdownx (django-markdown)

--- newmerology/urls.py
+++ newmerology/urls.py
@@ -1,9 +1,6 @@
 from django.conf.urls import include, url
 from calculator import urls as calc_urls
 
-from markdownx import urls as markdownx
-
 urlpatterns = [
     url(r'', include(calc_urls, namespace='calc')),
-    url(r'^markdownx/', include(markdownx))
 ]
